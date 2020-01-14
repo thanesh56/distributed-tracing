9c785ac32d2de952f8a5fedbcab7dac8d05f0b98
Thaneshwar Sahu <thaneshwar.sahu@globallogic.com>
2020-01-14 21:56:25 +0530

    added book module

---
+++ gl-book/src/main/java/com/gl/loggly/glbook/GlBookApplication.java
@@ -0,0 +1,13 @@
+package com.gl.loggly.glbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlBookApplication.class, args);
+	}
+
+}
---
+++ gl-book/src/test/java/com/gl/loggly/glbook/GlBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.loggly.glbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
